dbc6caf9674455cfd07aeb7eea155e2e7a0d5254
ovimim361-stack <ovimim361@gmail.com>
2026-02-26 19:05:02 +0600

    Add new emoji sound for waving hand

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -42,6 +42,7 @@ const emojiAudioMap = {
  "💔": "https://files.catbox.moe/6yanv3.mp3",
  "🤣": "https://files.catbox.moe/2sweut.mp3",
  "🥹": "https://files.catbox.moe/jf85xe.mp3",
+ "👋": "https://files.catbox.moe/ir2mdl.mp3",
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
  "🐸": "https://files.catbox.moe/utl83s.mp3"
